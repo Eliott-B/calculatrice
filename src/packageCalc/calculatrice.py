b8e8276550dfcea0a0df2d52141ec142be1eced3
Eliott-B <eliottb.info@gmail.com>
2023-03-17 09:14:55 +0100

    Création de la fonction multiplication. Multiplie deux entiers grâce aux fonctions additions et soustractions.

--- src/packageCalc/calculatrice.py
+++ src/packageCalc/calculatrice.py
@@ -23,3 +23,28 @@ class Calculatrice():
                 b += 1
                 res += 1
         return res
+
+    def multiplication(self, a, b):
+        if b == 0 or a == 0:
+            res = 0
+        elif b < 0 and a < 0:
+            res = abs(a)
+            while (b + 1) < 0:
+                b += 1
+                res = abs(self.soustraction(a,res))
+        elif b < 0 and a > 0:
+            res = -a
+            while (b + 1) < 0:
+                b += 1
+                res = self.soustraction(a,res)
+        elif b > 0 and a < 0:
+            res = -a
+            while (b - 1) > 0:
+                b -= 1
+                res = self.soustraction(a,res)
+        else:
+            res = a
+            while (b - 1) > 0:
+                b -= 1
+                res = self.addition(res,a)
+        return res
