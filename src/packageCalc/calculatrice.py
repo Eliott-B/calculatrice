a60583e0fb977461936d3ea4d83885800c89e743
Eliott-B <eliottb.info@gmail.com>
2023-03-17 09:13:03 +0100

    Création de la fonction soustraction. Soustrait deux entiers.

--- src/packageCalc/calculatrice.py
+++ src/packageCalc/calculatrice.py
@@ -11,3 +11,15 @@ class Calculatrice():
                 b += 1
                 res -= 1
         return res
+    
+    def soustraction(self, a, b):
+        res = a
+        if b >= 0:
+            while (b - 1) >= 0:
+                b -= 1
+                res -= 1
+        elif b < 0:
+            while (b + 1) <= 0:
+                b += 1
+                res += 1
+        return res
