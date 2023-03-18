53a86290f538a701e65333195bb9cb0caf9ad286
444chak <102913424+444chak@users.noreply.github.com>
2023-03-18 02:16:07 +0100

    Ajout du test de la soustraction

--- test/unit/test_calculatrice.py
+++ test/unit/test_calculatrice.py
@@ -19,5 +19,18 @@ class TestCalculatrice(unittest.TestCase):
                 obtenu = self.calculatrice.addition(a, b)
                 self.assertEqual(attendu, obtenu, "test case %d" % (index+1))
     
+    def test_soustraction(self):
+        a_data = [2,-1, 2,-5,5,0,-5, 0, 0, 5, -5]
+        b_data = [3,2,-1, -1, 0,5,0,-5, 0, 4, -6]
+        attendu_data = [-1,-3, 3,-4,5,-5,-5,5,0, 1,1]
+
+        for index in range(len(attendu_data)):
+            a = a_data[index]
+            b = b_data[index]
+            attendu = attendu_data[index]
+            with self.subTest(a=a, b=b, attendu=attendu):
+                obtenu = self.calculatrice.soustraction(a, b)
+                self.assertEqual(attendu, obtenu, "test case %d" % (index + 1))
+
 if __name__ == '__main__':
     unittest.main()
